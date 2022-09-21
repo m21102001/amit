cdb64e383b5343ac9ec67611d922095209333433
Abdallohmostafa <102461636+Abdallohmostafa@users.noreply.github.com>
2022-09-22 01:19:10 +0200

    toggle photos

---
+++ javascript.js
@@ -0,0 +1,12 @@
+
+ $(document).ready(function(){
+  $('#btn1').on("click",function(e){
+  $('#img1').toggle('slow');
+  }); 
+  $('#btn2').on("click",function(e){
+  $('#img2').toggle('slow');
+  }); 
+  $('#btn3').on("click",function(e){
+  $('#img3').toggle('slow');
+  }); 
+  });
